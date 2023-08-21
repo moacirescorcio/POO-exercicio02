a12449c364e0a3fb0fc6c25ea07232cad6f597aa
Moacir <89947549+moacirescorcio@users.noreply.github.com>
2023-08-21 09:46:24 -0300

    Add files via upload

---
+++ 07.js
@@ -0,0 +1,8 @@
+"use strict";
+//variáveis
+let nome = 'Ely';
+let pagamento = 120.56;
+let linguagem = 'TypeScript';
+//interpolação
+let frase = `Meu nome é ${nome}\n My payment time is ${pagamento}\n And\n my preffered language is ${linguagem}`;
+console.log(frase);
---
+++ 07.ts
@@ -0,0 +1,9 @@
+//variáveis
+let nome: string = 'Ely';
+let pagamento: number = 120.56;
+let linguagem: string = 'TypeScript';
+
+//interpolação
+let frase: string = `Meu nome é ${nome}\n My payment time is ${pagamento}\n And\n my preffered language is ${linguagem}`
+
+console.log(frase)
